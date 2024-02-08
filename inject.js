23ac595ba96dff016f6f845f5b6a799f26819541
arm-out <arminsuraj@gmail.com>
2024-02-08 12:41:23 -0800

    toggle fullscreen mode on ad

--- inject.js
+++ inject.js
@@ -16,11 +16,7 @@ window.setInterval(() => {
 		return;
 	}
 
-	console.log("{a}: mainVideo", mainVideo);
-	console.log("{a}: previewVideo", previewVideo);
-
 	const adPlaying = previewVideo && !previewVideo.paused;
-	console.log("{a}: adPlaying", adPlaying);
 
 	if (adPlaying && state === "no-ad") {
 		state = "ad";
@@ -28,20 +24,34 @@ window.setInterval(() => {
 		previewVideo.muted = false;
 		previewVideo.volume = mainVideo.volume;
 		mainVideo.muted = true;
-
 		previewVideo.controls = true;
-
-		const mainBox = mainVideo.getBoundingClientRect();
-		const previewBox = previewVideo.getBoundingClientRect();
-		const scale = mainBox.width / previewBox.width;
-
-		// const previewTransform = `translate(-100px, 0px)`;
-		// previewVideo.style.transform = previewTransform;
-		// previewVideo.style.zIndex = 2147483646;
 
 		const mainTransform = `scale(0)`;
 		mainVideo.style.transform = mainTransform;
 		mainVideo.style.zIndex = 0;
+
+		if (document.fullscreenElement) {
+			// Main vid playing in fullscreen
+			console.log("{a}: main vid playing in fullscreen");
+
+			// Exit current fullscreen ad
+			if (document.exitFullscreen) document.exitFullscreen();
+			else if (document.mozCancelFullScreen)
+				document.mozCancelFullScreen();
+			else if (document.webkitCancelFullScreen)
+				document.webkitCancelFullScreen();
+			else if (document.msExitFullscreen) document.msExitFullscreen();
+
+			// Fullscreen preview vid
+			if (previewVideo.requestFullscreen)
+				previewVideo.requestFullscreen();
+			else if (previewVideo.mozRequestFullScreen)
+				previewVideo.mozRequestFullScreen();
+			else if (previewVideo.webkitRequestFullscreen)
+				previewVideo.webkitRequestFullscreen();
+			else if (previewVideo.msRequestFullscreen)
+				previewVideo.msRequestFullscreen();
+		}
 	} else if (!adPlaying && state === "ad") {
 		state = "no-ad";
 		console.log("{a}: ad finished");
@@ -50,5 +60,25 @@ window.setInterval(() => {
 		previewVideo.style.removeProperty("transform");
 		mainVideo.style.removeProperty("transform");
 		mainVideo.style.removeProperty("z-index");
+
+		if (document.fullscreenElement) {
+			// Ad vid playing in fullscreen
+			console.log("{a}: minimizing preview");
+			if (document.exitFullscreen) document.exitFullscreen();
+			else if (document.mozCancelFullScreen)
+				document.mozCancelFullScreen();
+			else if (document.webkitCancelFullScreen)
+				document.webkitCancelFullScreen();
+			else if (document.msExitFullscreen) document.msExitFullscreen();
+
+			// Fullscreen main vid
+			if (mainVideo.requestFullscreen) mainVideo.requestFullscreen();
+			else if (mainVideo.mozRequestFullScreen)
+				mainVideo.mozRequestFullScreen();
+			else if (mainVideo.webkitRequestFullscreen)
+				mainVideo.webkitRequestFullscreen();
+			else if (mainVideo.msRequestFullscreen)
+				mainVideo.msRequestFullscreen();
+		}
 	}
 }, 500);
